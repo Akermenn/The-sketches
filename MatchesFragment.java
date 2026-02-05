084eb25f587abdc91fb4803756eaf2df904d8371
Akermenn <Andrey46042@yandex.ru>
2026-02-05 12:05:19 +0300

    Add files via upload

---
+++ MatchesFragment.java
@@ -0,0 +1,35 @@
+package com.example.sstatsmatches.ui.matches;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.sstatsmatches.databinding.FragmentMatchesBinding;
+
+public class MatchesFragment extends Fragment {
+
+    private FragmentMatchesBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentMatchesBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
+
+
+src/main/java/com/example/sstatsmatches/ui/matches/MatchesFragment.java
